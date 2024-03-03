1d362e08d35fe974fa91123b2b3891bebb0a47d0
pkc2610 <phoebekayc@gmail.com>
2024-03-02 17:07:01 -0700

    I think I did it!

--- discount.py
+++ discount.py
@@ -1,24 +1,20 @@
-#gets a customer’s subtotal as input and gets the current day of the week from your computer’s 
-#operating system. Your program must not ask the user to enter the day of the week. Instead, 
-#it must get the day of the week from your computer’s operating system.
-#If the subtotal is $50 or greater and today is Tuesday or Wednesday, your program must subtract 
-#10% from the subtotal. Your program must then compute the total amount due by adding sales tax 
-#of 6% to the subtotal. Your program must print the discount amount if applicable, the sales tax 
-#amount, and the total amount due.
-#Your program asks the user for the subtotal but does not ask the user for the day of the week. 
-#Your program gets the day of the week from your computer’s operating system.
-#Your program correctly computes and prints the discount amount if applicable.
-#Your program correctly computes and prints the sales tax amount and the total amount due.
-
-# Import the datetime class from the datetime
-# module so that it can be used in this program.
 from datetime import datetime
 
-current_date_and_time = datetime.now()
+d = datetime.now()
 
-day_of_week = current_date_and_time.weekday()
+subtotal = float(input("Please enter your subtotal: "))
+discount=subtotal*.9
+rabat=subtotal*.1
+tax=discount*.06
 
-discount = net_total*.1
+salestax=subtotal*.06
 
-if day_of_week == 1: 
-    print(discount)
+if d == (2, 3) and subtotal >= 50:
+    print("Your discount is 10%!")
+    print(f"Your subtotal is: ${discount:.2f}")
+    print(f"Your discount amount is: ${rabat:.2f}")
+    print(f"Sales Tax: ${tax:.2f}")
+    print(f"Your total is: ${discount+tax:.2f}")
+elif d !=(2, 3):
+    print(f"Sales Tax: ${salestax:.2f}")
+    print(f"Your total is: ${salestax+subtotal:.2f}")
