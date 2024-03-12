fb094d9a9436a1fb645144fbeeb31f3f5082549d
pkc2610 <phoebekayc@gmail.com>
2024-03-11 21:15:51 -0700

    ought to start leaving commas

--- sentences.py
+++ sentences.py
@@ -1,23 +1,33 @@
 import random
 
+#"articles singular"
 articless = ["a", "the", "an", "one", "that"]
 
+#"articles plural"
 articlesp = ["the", "some", "those", "a few"]
 
+#"nouns singular"
 nounss = ["bird", "boy", "car", "cat", "child", "dog", "girl", "man", "rabbit", "woman"]
 
+#"nouns plural"
 nounsp = ["birds", "boys", "cars", "cats", "children", "dogs", "girls", "men", "rabbits", "women"]
 
+#"verbs singular past tense"
 vsinglepast = ["drank", "ate", "grew", "laughed", "thought", "ran", "slept", "talked", "walked", "wrote"]
 
+#"verbs singular present tense"
 vsinglepres = ["drinks", "eats", "grows", "laughs", "thinks", "runs", "sleeps", "talks", "walks", "writes"]
 
+#"verbs singular future tense"
 vsinglefut = ["will drink", "will eat", "will grow", "will laugh", "will think", "will run", "will sleep", "will talk", "will walk", "will write"]
 
+#"verbs plural past tense"
 vpluralpast = ["drank", "ate", "grew", "laughed", "thought", "ran", "slept", "talked", "walked", "wrote"]
 
+#"verbs plural present tense"
 vpluralpres = ["drink", "eat", "grow", "laugh", "think", "run", "sleep", "talk", "walk", "write"]
 
+#"verbs plural future tense"
 vpluralfut = ["will drink", "will eat", "will grow", "will laugh", "will think", "will run", "will sleep", "will talk", "will walk", "will write"]
 
 
@@ -28,9 +38,9 @@ cap_word = word.capitalize()
 def get_determiner(quantity):
 
     if quantity == 1:
-        words = ["a", "one", "the"]
-    else:
-        words = ["some", "many", "the"]
+        return articless
+    else: 
+        return articlesp
 
     word = random.choice(words)
     return word
