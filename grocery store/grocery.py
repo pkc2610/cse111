4b949fd48f9dbab02f8372963cf11ea09a7f8647
pkc2610 <phoebekayc@gmail.com>
2024-03-30 21:21:04 -0700

    I think I've got this (which might indicate otherwise but we're getting somewhere)

--- grocery store/grocery.py
+++ grocery store/grocery.py
@@ -5,4 +5,10 @@ product_dictionary = {}
 with open("products.csv", "request.csv") as infile:
     
     will_read = csv.reader(infile)
-    
+
+    next(will_read)
+
+    for row in will_read:
+        code = row(0)
+        product = row(1)
+        price = row(2)
