603d485570d379796dd3e982e5c1dbb2d57358fb
pkc2610 <phoebekayc@gmail.com>
2024-04-06 14:28:06 -0700

    starting the final project!

---
+++ Phoebe's_Schwanky_To_Do_List.py
@@ -0,0 +1,6 @@
+#instead of having a separate txt or csv file, I found that I liked having the list out in front of me;
+#and me being who I am, I didn't think a list of less than 20 things needed a separate txt file. Should
+#the list get long enough, I'd probably make it into a csv file
+
+tasks = ["find Tom Roombadil" , "feed Echo" , "swaddle the baby" , "make sandwiches" , "buy shrimp chips"]
+
