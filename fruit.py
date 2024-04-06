fdb4ec66d9aa117adcd9e52a972cfd92730a3a46
pkc2610 <phoebekayc@gmail.com>
2024-04-06 04:22:37 -0700

    I'm coding at 4 AM with my 3-week old nephew 🥰

---
+++ fruit.py
@@ -0,0 +1,7 @@
+def main():
+    # Create and print a list named fruit.
+    fruit_list = ["pear", "banana", "apple", "mango"]
+    print(f"original: {fruit_list}")
+
+    fruit_list.reverse()
+    
